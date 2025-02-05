be6fa16232e419e1c4f4e21046303d6cb9521b1c
WillSantosNe <williamsantosstella@gmail.com>
2025-02-04 23:30:51 -0300

    Create CustomUser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,70 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user manager to handle user creation.
+    """
+
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create and return a regular user with an email and password.
+        
+        Args:
+            email (str): The email address for the user.
+            password (str, optional): The password for the user. Defaults to None.
+            **extra_fields: Additional fields for the user model.
+
+        Raises:
+            ValueError: If the email is not provided.
+
+        Returns:
+            CustomUser: The created user instance.
+        """
+        if not email:
+            raise ValueError("The email field is required.")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Create and return a superuser with the given email and password.
+        
+        Args:
+            email (str): The email address for the superuser.
+            password (str, optional): The password for the superuser. Defaults to None.
+            **extra_fields: Additional fields for the user model.
+
+        Returns:
+            CustomUser: The created superuser instance.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user model that extends AbstractBaseUser and PermissionsMixin.
+    """
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    password = models.CharField(max_length=128)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
