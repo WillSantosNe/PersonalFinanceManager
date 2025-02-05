a5fb3baa739811470db87616ea10bade674cc4b5
WillSantosNe <williamsantosstella@gmail.com>
2025-02-04 22:46:25 -0300

    Update project settings in settings.py

--- finance_project/settings.py
+++ finance_project/settings.py
@@ -37,6 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Django Rest Framework e JWT
+    'rest_framework',
+    'rest_framework_simplejwt',
+    
+    # Projects apps
+    'accounts',
+    'categories',
+    'transactions',
+    'goals',
+    'reports',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +132,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
